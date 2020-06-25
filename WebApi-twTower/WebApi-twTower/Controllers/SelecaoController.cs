65d80ae1e3bb9da2c1b72dc72f36b7dc0a23ecca
Marco F. Basile <uyguy66@hotmail.com>
2020-06-24 22:45:01 -0300

    adicionando o primeiro metodo list, tudo funcionando

---
+++ WebApi-twTower/WebApi-twTower/Controllers/SelecaoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi_twTower.Domains;
+using WebApi_twTower.Repositories;
+
+namespace WebApi_twTower.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class SelecaoController : ControllerBase
+    {
+        SelecaoRepository selecao = new SelecaoRepository();
+
+        [HttpGet]
+        public IActionResult listSelecao()
+        {
+            return Ok(selecao.ListarSelecao());
+        }
+    }
+}
